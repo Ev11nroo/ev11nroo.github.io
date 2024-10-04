effbfb4e79ef30200ff5972d38323f0c2c843acd
roo <146498770+Ev11nroo@users.noreply.github.com>
2024-10-03 21:49:48 -0400

    add reversing animation

--- random/animationtest/index.js
+++ random/animationtest/index.js
@@ -1,3 +1,8 @@
 function expand(itemNum) {
+    if (document.getElementById(itemNum).style.animationName == "expand") {
+        document.getElementById(itemNum).style.animationName = "reverse";
+        return 0;
+    }
+
     document.getElementById(itemNum).style.animationName = "expand";
 }
