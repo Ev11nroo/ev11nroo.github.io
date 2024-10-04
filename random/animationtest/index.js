d305a067068866ec3cd27cea562b331abb8759a5
roo <146498770+Ev11nroo@users.noreply.github.com>
2024-10-04 17:36:56 -0400

    check for content once

--- random/animationtest/index.js
+++ random/animationtest/index.js
@@ -19,39 +19,39 @@ function rotate(arrowNum) {
 }
 
 function toggleContent(num) {
-    if (Content1Shown == true) {
-        Content1Shown = false;
-        document.getElementById("content1").hidden = true;
-        document.getElementById('content1').innerHTML = "";
-        return 0;
-    }
-
-    if (Content2Shown == true) {
-        Content2Shown = false;
-        document.getElementById("content2").hidden = true;
-        document.getElementById('content2').innerHTML = "";
-        return 0;
-    }
-
-    if (Content3Shown == true) {
-        Content3Shown = false;
-        document.getElementById("content3").hidden = true;
-        document.getElementById('content3').innerHTML = "";
-        return 0;
-    }
-    
     switch (num) {
         case 1:
+            if (Content1Shown == true) {
+                Content1Shown = false;
+                document.getElementById("content1").hidden = true;
+                document.getElementById('content1').innerHTML = "";
+                return 0;
+            }
+
             Content1Shown = true;
             document.getElementById("content1").hidden = false;
             document.getElementById('content1').innerHTML = "this is indeed something that is possible with HTML (im too lazy to create an image)";
             break;
         case 2:
+            if (Content2Shown == true) {
+                Content2Shown = false;
+                document.getElementById("content2").hidden = true;
+                document.getElementById('content2').innerHTML = "";
+                return 0;
+            }
+
             Content2Shown = true;
             document.getElementById("content2").hidden = false;
             document.getElementById("content2").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat pretium purus suscipit suscipit. Duis semper mauris et porttitor varius. Ut aliquam semper dapibus. Sed et dapibus orci. Suspendisse potenti. Fusce sit amet lectus libero. Proin pellentesque elementum vestibulum. Aliquam viverra lacus vitae convallis dignissim. Maecenas nec feugiat lorem. In eget purus quis risus venenatis imperdiet id at quam. Nullam sit amet porta urna. Vivamus mattis, elit at imperdiet vulputate, lorem diam molestie libero, ut pulvinar orci massa vitae libero. Sed risus velit, ultrices a purus efficitur, vestibulum posuere nisi. Nunc auctor odio vitae neque gravida, sit amet tincidunt enim dictum.";
             break;
         case 3:
+            if (Content3Shown == true) {
+                Content3Shown = false;
+                document.getElementById("content3").hidden = true;
+                document.getElementById('content3').innerHTML = "";
+                return 0;
+            }
+
             Content3Shown = true;
             document.getElementById("content3").hidden = false;
             document.getElementById("content3").innerHTML = "auosebfuiesdigvdfxzbgiurszhfzsebhfkeasjbfhghfveszgbbifuwhfvqwevyuofqefveasdzfzbsdfmvbefawugevifqw4eyofuwqegfyeuovgfapyesZFpZPEFuvghawes";
