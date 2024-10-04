2e2211748ba6c711004a58b98471e907eeb17601
roo <146498770+Ev11nroo@users.noreply.github.com>
2024-10-03 22:42:09 -0400

    add content to animationtest

--- random/animationtest/index.js
+++ random/animationtest/index.js
@@ -15,3 +15,20 @@ function rotate(arrowNum) {
 
     document.getElementById(arrowNum).style.animationName = "rotate";
 }
+
+function addContent(num) {
+    switch (num) {
+        case 1:
+            document.getElementById("content1").hidden = false;
+            document.getElementById('content1').innerHTML = "this is indeed something that is possible with HTML (im too lazy to create an image)";
+            break;
+        case 2:
+            document.getElementById("content2").hidden = false;
+            document.getElementById("content2").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat pretium purus suscipit suscipit. Duis semper mauris et porttitor varius. Ut aliquam semper dapibus. Sed et dapibus orci. Suspendisse potenti. Fusce sit amet lectus libero. Proin pellentesque elementum vestibulum. Aliquam viverra lacus vitae convallis dignissim. Maecenas nec feugiat lorem. In eget purus quis risus venenatis imperdiet id at quam. Nullam sit amet porta urna. Vivamus mattis, elit at imperdiet vulputate, lorem diam molestie libero, ut pulvinar orci massa vitae libero. Sed risus velit, ultrices a purus efficitur, vestibulum posuere nisi. Nunc auctor odio vitae neque gravida, sit amet tincidunt enim dictum.";
+            break;
+        case 3:
+            document.getElementById("content3").hidden = false;
+            document.getElementById("content3").innerHTML = "auosebfuiesdigvdfxzbgiurszhfzsebhfkeasjbfhghfveszgbbifuwhfvqwevyuofqefveasdzfzbsdfmvbefawugevifqw4eyofuwqegfyeuovgfapyesZFpZPEFuvghawes";
+            break;
+    }
+}
