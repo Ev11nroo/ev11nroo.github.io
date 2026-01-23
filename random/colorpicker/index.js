b356100d9e68c79c7a9c00abdc7bb816f4c8c060
roo <146498770+Ev11nroo@users.noreply.github.com>
2026-01-22 23:10:47 -0500

    colorpicker: update rgb values

--- random/colorpicker/index.js
+++ random/colorpicker/index.js
@@ -1,10 +1,22 @@
+function turnPartialHexToDecimal(partHex) {
+    const hex = `0x${partHex}`;
+    console.log(hex);
+    console.log(Number(hex));
+
+    return Number(hex);
+}
+
 function setColorValues(event) {
     const value = String(event.target.value);
     const hexElement = document.getElementById("hex");
+    const rgbElement = document.getElementById("rgb");
 
     const hexValue = value.replace("#", "");
+    const hexSplit = hexValue.match(/.{1,2}/g); // stack overflow or something regex
+    const rgbValue = `${turnPartialHexToDecimal(hexSplit[0])}, ${turnPartialHexToDecimal(hexSplit[1])}, ${turnPartialHexToDecimal(hexSplit[2])}`;
 
-    hexElement.innerHTML = `hex: ${hexValue}`
+    hexElement.innerHTML = `hex: ${hexValue}`;
+    rgbElement.innerHTML = `rgb: ${rgbValue}`;
 }
 
 addEventListener("DOMContentLoaded", () => {
